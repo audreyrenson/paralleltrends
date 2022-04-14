4f90a0e3aae871bb128f135f6b5f7c0167adc96b
audreyrenson <audrey.o.renson@gmail.com>
2022-04-14 14:26:20 -0400

    document generate_parameters() return value and correctly reference stats:: functions

--- R/generate_parameters.R
+++ R/generate_parameters.R
@@ -12,7 +12,7 @@
 #' @param Beta_YU numeric. constant coefficient for effect of unmeasured, time-invariant, binary covariate U in Y models
 #' @param range_ymeans numeric vector of length 2. minimum and maximum values for marginal means of Y
 #'
-#' @return
+#' @return list of matrices, one for each variable to be generated (U, W1, W2, A, Y). Each matrix has T+1 rows and a different number of columns corresponding to the coefficients of a (generalized) linear model. The columns names refer to the term in that model.
 #' @export
 #'
 #'
@@ -25,21 +25,21 @@ generate_parameters <- function(Tt,
                                 Beta_YU = 0.1) {
 
   Beta_U = -log(1 / 0.5 - 1)
-  mean_U = plogis(Beta_U)
+  mean_U = stats::plogis(Beta_U)
 
   # matrices of parameters
-  Beta_W1 = matrix( rnorm(n = 2*(Tt+1), mean =mu_Beta_W1, sd = sd_Beta_W1), nrow = Tt + 1, byrow = TRUE)# 2 because terms for A + intercept
-  Beta_W2 = matrix( rnorm(n = 2*(Tt+1), mean =mu_Beta_W2, sd = sd_Beta_W2), nrow = Tt + 1, byrow = TRUE)# 2 because terms for A + intercept
-  Beta_A = matrix( rnorm(n = 5*(Tt+1), mean =mu_Beta_A, sd = sd_Beta_A), nrow = Tt + 1, byrow = TRUE)# 5 because terms for U, L, W, W^2, + intercept
-  Beta_Y = matrix( rnorm(n = 6*(Tt+1), mean =mu_Beta_Y, sd = sd_Beta_Y), nrow = Tt + 1, byrow = TRUE)# 6 because terms for U, L, W, W^2, A + intercept
+  Beta_W1 = matrix( stats::rnorm(n = 2*(Tt+1), mean =mu_Beta_W1, sd = sd_Beta_W1), nrow = Tt + 1, byrow = TRUE)# 2 because terms for A + intercept
+  Beta_W2 = matrix( stats::rnorm(n = 2*(Tt+1), mean =mu_Beta_W2, sd = sd_Beta_W2), nrow = Tt + 1, byrow = TRUE)# 2 because terms for A + intercept
+  Beta_A = matrix( stats::rnorm(n = 5*(Tt+1), mean =mu_Beta_A, sd = sd_Beta_A), nrow = Tt + 1, byrow = TRUE)# 5 because terms for U, L, W, W^2, + intercept
+  Beta_Y = matrix( stats::rnorm(n = 6*(Tt+1), mean =mu_Beta_Y, sd = sd_Beta_Y), nrow = Tt + 1, byrow = TRUE)# 6 because terms for U, L, W, W^2, A + intercept
 
   Beta_Y[,2] = Beta_YU #constant effect of U0 = part of sufficient conditions for parallel trends
 
   # balancing intercepts
-  mean_Y = runif(Tt + 1, min = range_ymeans[1], max = range_ymeans[2])
-  mean_W1 = runif(Tt + 1, min = 0.1, max = 0.5)
+  mean_Y = stats::runif(Tt + 1, min = range_ymeans[1], max = range_ymeans[2])
+  mean_W1 = stats::runif(Tt + 1, min = 0.1, max = 0.5)
   mean_W2 = 0
-  mean_A_conditional = runif(Tt + 1, min = 1/(Tt^2), max = 1/(Tt^2) + 1/Tt) # this is a function of T to avoid very small pr(A_t=0) for large t
+  mean_A_conditional = stats::runif(Tt + 1, min = 1/(Tt^2), max = 1/(Tt^2) + 1/Tt) # this is a function of T to avoid very small pr(A_t=0) for large t
   mean_A_marginal = 1 - cumprod(1 - mean_A_conditional)  # basically a kaplan meier risk function
 
   Beta_Y[, 1] = mean_Y - Beta_Y[, 2]*mean_U - Beta_Y[, 3]*mean_W1 - (Beta_Y[,4] + Beta_Y[,5])*mean_W2 - Beta_Y[,6]*mean_A_marginal
